932b63feb4f0f9d3b4d4a1dd2739e573065e02fa
Richelle Cripe <richellecripe@gmail.com>
2015-10-09 11:13:58 -0600

    adding server

---
+++ app.js
@@ -0,0 +1,30 @@
+// --- app.js is the scaffolding for the App ---\\
+
+
+// --- Require --- \\
+var express = require('express')
+var bodyParser = require('body-parser')
+
+
+// --- Create Express App Object --- \\
+var app = express()
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
+// --- Serve Static Files --- \\
+app.use( express.static(__dirname + '/public') )
+
+// --- Routes --- \\
+
+app.get( '/', function(req, res){
+	res.sendFile('/html/index.html', {root : './public'})
+})
+
+// --- Create Port & Listen for Connections --- \\
+var port = 3000
+
+
+app.listen(port, function(){
+	console.log('Server running on port: ' + port)
+})
